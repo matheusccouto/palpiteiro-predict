df58045f9cd3183b666c676a8e07e0f481582f45
Matheus Couto <37025583+matheusccouto@users.noreply.github.com>
2022-10-06 00:06:50 +0000

    Update README

--- diagrams/make_diagrams.py
+++ diagrams/make_diagrams.py
@@ -5,22 +5,17 @@
 import os
 
 from diagrams import Cluster, Diagram, Edge
-from diagrams.aws.network import APIGateway
+from diagrams.aws.integration import StepFunctions
 from diagrams.custom import Custom
 from diagrams.gcp.analytics import Bigquery
-from diagrams.gcp.compute import Functions
 from diagrams.gcp.storage import Storage
-from diagrams.onprem.ci import GithubActions
-from diagrams.onprem.vcs import Git, Github
-from diagrams.programming.language import Python
-
 
 THIS_DIR = os.path.dirname(__file__)
 ICONS_DIR = os.path.join(THIS_DIR, "icons")
 
 
 with Diagram(
-    filename=os.path.join(THIS_DIR, "overview"),
+    filename=os.path.join(THIS_DIR, "architecture"),
     show=False,
     curvestyle="curved",
 ):
@@ -32,7 +27,7 @@ with Diagram(
     with Cluster("Scheduled GitHub Actions"):
         lgbm = Custom("lgbmranker", os.path.join(ICONS_DIR, "lgbm.png"))
         optuna = Custom("optuna", os.path.join(ICONS_DIR, "optuna.png"))
-        draft = APIGateway("draft api")
+        draft = StepFunctions("draft")
 
         fct_player >> Edge(label="tune") >> optuna
         optuna >> Edge(label="train") >> lgbm
@@ -43,19 +38,3 @@ with Diagram(
         lgbm = Custom("lgbmranker", os.path.join(ICONS_DIR, "lgbm.png"))
         storage >> Edge(label="load") >> lgbm
         lgbm >> Edge(label="predict") >> dim_player_last
-
-
-with Diagram(
-    "\nFlow",
-    filename=os.path.join(THIS_DIR, "flow"),
-    show=False,
-    curvestyle="curved",
-):
-    gh_pr = Github("pull request to main")
-    gh_merge = Github("merge to main")
-
-    Git("push to dev") >> gh_pr
-    gh_pr >> GithubActions("testing") >> Python("pytest") >> gh_merge
-    gh_pr >> GithubActions("linting") >> Python("pylint") >> gh_merge
-    gh_merge >> GithubActions("deploy") >> Functions("Deploy")
-    gh_merge >> GithubActions("train") >> Storage("Dump")
